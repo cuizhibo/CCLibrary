6253d890c745dee8d09d253e2d0da01b7b2a229d
cuizhibo <cuizhibo@51talk.com>
2018-05-07 18:27:22 +0800

    Initial Commit

---
+++ CCLibraryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCLibraryDemo
+//
+//  Created by Choi on 2018/5/7.
+//  Copyright © 2018年 51Talk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCLibraryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCLibraryDemo
+//
+//  Created by Choi on 2018/5/7.
+//  Copyright © 2018年 51Talk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
