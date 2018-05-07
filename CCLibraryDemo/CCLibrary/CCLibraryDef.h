d72f98bb5d23b09e7e674bd4923f0dc88b62bd67
cuizhibo <cuizhibo@51talk.com>
2018-05-07 19:19:48 +0800

    添加头文件

---
+++ CCLibraryDemo/CCLibrary/CCLibraryDef.h
@@ -0,0 +1,13 @@
+//
+//  CCLibraryDef.h
+//  CCLibraryDemo
+//
+//  Created by Choi on 2018/5/7.
+//  Copyright © 2018年 51Talk. All rights reserved.
+//
+
+#ifndef CCLibraryDef_h
+#define CCLibraryDef_h
+
+
+#endif /* CCLibraryDef_h */
